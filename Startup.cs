39fe64f98e0e22c44d4dd761f1b3d0642ffe3a32
acltearr <burroughskaelen@gmail.com>
2021-04-07 14:32:09 -0400

    Added to database connection string

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace RestAPI
                     // "server=localhost;port=3306;database=RailsApp_development;uid=codeboxx;password=Codeboxx1*",
 
                     // Live Site Connection
-                    "server=codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com;database=KaelenBurroughs;uid=codeboxx;password=Codeboxx1!",
+                    "server=codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com;database=KaelenBurroughs;uid=codeboxx;password=Codeboxx1!;SslMode=none",
 
                     new MySqlServerVersion(new Version(8, 0, 21)),
                         mySqlOptions => mySqlOptions
